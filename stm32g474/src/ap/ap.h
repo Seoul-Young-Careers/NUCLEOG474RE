a7ed7c6dfba9c4bad7a6c7e647a1a778a7a3a27d
turn-world <whdydwns0604@gmail.com>
2026-05-10 16:05:57 +0900

    Initial commit

---
+++ stm32g474/src/ap/ap.h
@@ -0,0 +1,17 @@
+/*
+ * ap.h
+ *
+ *  Created on: Dec 6, 2020
+ *      Author: baram
+ */
+
+#ifndef SRC_AP_AP_H_
+#define SRC_AP_AP_H_
+
+#include "hw.h"
+
+void apInit(void);
+void apMain(void);
+
+
+#endif /* SRC_AP_AP_H_ */
---
+++ stm32g474/src/main.h
@@ -0,0 +1,13 @@
+/*
+ * main.h
+ *
+ *  Created on: May 9, 2026
+ *      Author: young
+ */
+
+#ifndef SRC_MAIN_H_
+#define SRC_MAIN_H_
+
+#include "ap.h"
+
+#endif /* SRC_MAIN_H_ */
