5fe76ec9a62ae5999614ef4d0d3aa0fdef2bc217
조용준 <127725768+turn-world@users.noreply.github.com>
2026-06-09 14:51:55 +0900

    Revert "Merge branch 'main' of https://github.com/Seoul-Young-Careers/NUCLEOG474RE"
    This reverts commit 2f53dc9c4dc8eddacb2dc5e751db513ec2fff5f7, reversing
    changes made to 6646a2acdef0fdbd0794d457781d19ec8e640437.

--- stm32g474/src/ap/app_sequence.c
+++ stm32g474/src/ap/app_sequence.c
@@ -190,7 +190,7 @@ static bool runStopSequence(void)
     return false;
   }
 
-  if(taskStepMotorMoveToHome(&cmd_id) != true)
+  if(taskStepMotorMoveToZero(&cmd_id) != true)
   {
     enterErrorState();
     return false;
@@ -261,7 +261,7 @@ static bool runStartSequence(void)
 
   setState(APP_SEQUENCE_STATE_MOVING_TO_END);
 
-  if(taskStepMotorMoveToEnd(&cmd_id) != true)
+  if(taskStepMotorMoveToFull(&cmd_id) != true)
   {
     enterErrorState();
     return false;
